30e4d160bb43531f79f78e1ff666e92974fa518b
Chet Husk <chusk3@gmail.com>
2019-09-26 07:40:57 -0700

    more work

--- memory-leaks/start.sh
+++ memory-leaks/start.sh
@@ -9,4 +9,4 @@ fi
 TFM=$1
 
 # start the app in the background with a particular name
-docker run --cap-add SYS_ADMIN -v "$(pwd)/dump":/dump --rm -p=5000:80 --name memoryleak_"$TFM" memory-leak:"$TFM" &
+docker run --cap-add SYS_ADMIN --cap-add SYS_PTRACE -v "$(pwd)/dump":/dump --rm -p=5000:80 --name memoryleak_"$TFM" memory-leak:"$TFM" &
