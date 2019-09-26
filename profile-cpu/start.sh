1bb3b61045c40752d23df2829cb2940b13088795
Chet Husk <chusk3@gmail.com>
2019-09-26 15:40:28 -0700

    finish slides

--- profile-cpu/start.sh
+++ profile-cpu/start.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 # note that we add the SYS_ADMIN capability here so that we can use `perf` if necessary
-docker run --cap-add SYS_ADMIN -v "$(pwd)/trace":/trace --rm --name profile-cpu profile-cpu &
+docker run --cap-add SYS_ADMIN --privileged -v "$(pwd)/trace":/trace --rm --name profile-cpu profile-cpu &
